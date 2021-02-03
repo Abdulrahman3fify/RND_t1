45c84d1cdabe8affaa6c10287de920d5d400bed5
Abdulrahman3fify <58623692+Abdulrahman3fify@users.noreply.github.com>
2021-02-03 20:38:08 +0200

    Update 2.js

--- answers/2.js
+++ answers/2.js
@@ -217,13 +217,13 @@ let json = [
   }
 ]
 const largestNumber = array => {
-let max=0;
+let max=0,id;
 for(let i=0; i< array.length - 1; i ++) {
 	if(array[i].amount > array[i + 1].amount && array[i].amount > max){
 	max = array[i].amount
+	id = array[i].id
   }
 }
-console.log(max)
-return max
+return id;
 };
 largestNumber(json);
